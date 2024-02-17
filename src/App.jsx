476046416f55c3d5ee68315831fa60562718faf0
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-17 13:30:43 +0530

    Added ids and ref

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,26 @@ const App = () => {
   }, []);
 
   return (
-    <div className="">
-      <div className="absolute h-screen top-0 left-0 bg-gray-200 font-spaceGrotesk z-10 w-full flex flex-col gap-10 tracking-tight">
-        <h1 className="text-9xl font-bold">FrontEnd Developer</h1>
-        <h1 className="text-9xl font-bold">Content Creator</h1>
-        <h1 className="text-9xl font-bold">Freelancer</h1>
+    <div ref={comp} className="relative">
+      <div
+        id="intro-slider"
+        className="absolute h-screen top-0 left-0 bg-gray-200 font-spaceGrotesk z-10 w-full flex flex-col gap-10 tracking-tight"
+      >
+        <h1 className="text-9xl font-bold" id="title-1">
+          FrontEnd Developer
+        </h1>
+        <h1 className="text-9xl font-bold" id="title-2">
+          Content Creator
+        </h1>
+        <h1 className="text-9xl font-bold" id="title-3">
+          Freelancer
+        </h1>
       </div>
       <div className="h-screen flex bg-gray-950 justify-center items-center">
-        <h1 className="font-bold font-spaceGrotesk text-9xl text-white">
+        <h1
+          id="welcome"
+          className="font-bold font-spaceGrotesk text-9xl text-white"
+        >
           Welcome.
         </h1>
       </div>
