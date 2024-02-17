63ba4a266eab455f8247ed3e2d77a19c6c0256b9
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-17 12:53:39 +0530

    Added text and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>sdfsd</div>;
+  return (
+    <div className="h-screen flex bg-gray-950 justify-center items-center">
+      <h1>sdfsd</h1>
+    </div>
+  );
 };
 
 export default App;
