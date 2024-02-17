08a9215e397bf3f907689afeddff68c2e808c4fe
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-17 13:20:26 +0530

    Added h1 for the animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="h-screen flex bg-gray-950 justify-center items-center">
-      <h1>sdfsd</h1>
+    <div className="">
+      <div className="absolute h-screen top-0 left-0 bg-gray-400 font-spaceGrotesk z-10 w-full flex flex-col gap-10 tracking-tight">
+        <h1 className="">FrontEnd Developer</h1>
+        <h1 className="">Content Creator</h1>
+        <h1 className="">Freelancer</h1>
+      </div>
+      <div className="h-screen flex bg-gray-950 justify-center items-center">
+        <h1 className="font-bold font-spaceGrotesk text-9xl text-white">
+          Welcome.
+        </h1>
+      </div>
     </div>
   );
 };
